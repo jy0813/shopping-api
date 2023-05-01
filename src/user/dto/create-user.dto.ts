962ef133adc933f1b6095b9f6d37a0ab5439b200
jy0813 <busbug3@gmail.com>
2023-05-01 12:16:53 +0900

    feat: valid 인증

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,6 +1,18 @@
+import { IsEmail, IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
+
 export class CreateUserDto {
+  @IsString()
+  @MinLength(7)
+  @MaxLength(18)
   userName: string;
+
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(7)
+  @MaxLength(18)
   password: string;
   isMarketing: boolean;
   isEvent: boolean;
