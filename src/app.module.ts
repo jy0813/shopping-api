02f1e21a144f17c7f77801ae3df3cccbcf3a58d6
jy0813 <busbug3@gmail.com>
2023-05-01 09:30:39 +0900

    feat: 환경변수 valid 체크

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from "@nestjs/config";
+import * as Joi from "@hapi/joi";
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    DatabaseModule
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.string().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DATABASE: Joi.string().required(),
+      }),
+    }),
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
